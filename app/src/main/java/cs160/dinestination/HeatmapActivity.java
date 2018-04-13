33f163305dbc8a85ec4b780e20ae763a62c68447
Adriana Babakanian <ababakanian@berkeley.edu>
2018-04-12 21:56:45 -0700

    added dialogs

--- app/src/main/java/cs160/dinestination/HeatmapActivity.java
+++ app/src/main/java/cs160/dinestination/HeatmapActivity.java
@@ -8,6 +8,6 @@ public class HeatmapActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_heatmap);
     }
 }
---
+++ app/src/main/java/cs160/dinestination/PreviewDialog.java
@@ -0,0 +1,22 @@
+package cs160.dinestination;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+
+public class PreviewDialog extends DialogFragment {
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        // Get the layout inflater
+        LayoutInflater inflater = getActivity().getLayoutInflater();
+        // Inflate and set the layout for the dialog
+        builder.setView(inflater.inflate(R.layout.dialog_preview, null));
+        return builder.create();
+    }
+}
