89e52e51fef4a8a729e184561909b88a0b36814b
Adriana Babakanian <ababakanian@berkeley.edu>
2018-04-12 21:20:18 -0700

    initial commit

---
+++ app/src/main/java/cs160/dinestination/MainActivity.java
@@ -0,0 +1,13 @@
+package cs160.dinestination;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
