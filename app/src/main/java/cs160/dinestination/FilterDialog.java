17a9ea950ce061e88ddd6654e9734227eebe219f
Adriana Babakanian <ababakanian@berkeley.edu>
2018-04-14 13:52:31 -0700

    alert dialog shows when click on filter button

--- app/src/main/java/cs160/dinestination/FilterDialog.java
+++ app/src/main/java/cs160/dinestination/FilterDialog.java
@@ -2,7 +2,8 @@ package cs160.dinestination;
 
 import android.app.AlertDialog;
 import android.app.Dialog;
-import android.app.DialogFragment;
+import android.support.v4.app.DialogFragment;
+import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -13,6 +14,17 @@ public class FilterDialog extends DialogFragment {
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setMessage(R.string.dialog_filters)
+                .setPositiveButton(R.string.fire, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // FIRE ZE MISSILES!
+                    }
+                })
+                .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // User cancelled the dialog
+                    }
+                });
         // Get the layout inflater
         LayoutInflater inflater = getActivity().getLayoutInflater();
         // Inflate and set the layout for the dialog
--- app/src/main/java/cs160/dinestination/MainActivity.java
+++ app/src/main/java/cs160/dinestination/MainActivity.java
@@ -1,13 +1,38 @@
 package cs160.dinestination;
 
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.logging.Filter;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button testFilterButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        testFilterButton = findViewById(R.id.test_filter_button);
+        setOnClickForTestFilterButton();
+
+    }
+
+    private void setOnClickForTestFilterButton() {
+        testFilterButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                FilterDialog filter = new FilterDialog();
+                filter.show(getSupportFragmentManager(), "filters");
+
+            }
+        });
     }
 }
