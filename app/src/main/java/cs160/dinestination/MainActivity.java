56bb471dd3bfa0b129a42189e30b22768c268fba
Adriana Babakanian <ababakanian@berkeley.edu>
2018-04-14 14:30:29 -0700

    functionality of bottom sheet works

--- app/src/main/java/cs160/dinestination/MainActivity.java
+++ app/src/main/java/cs160/dinestination/MainActivity.java
@@ -1,5 +1,8 @@
 package cs160.dinestination;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomSheetBehavior;
+import android.support.design.widget.BottomSheetDialog;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -7,20 +10,35 @@ import android.support.v4.app.DialogFragment;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
 
 import java.util.logging.Filter;
 
 public class MainActivity extends AppCompatActivity {
 
     Button testFilterButton;
+    BottomSheetDialog mBottomSheetDialog;
+    BottomSheetBehavior sheetBehavior;
+    LinearLayout layoutBottomSheet;
+    Button testButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // old alert dialog for filters
         testFilterButton = findViewById(R.id.test_filter_button);
         setOnClickForTestFilterButton();
+
+        // hook up UI elements
+        layoutBottomSheet = findViewById(R.id.bottom_sheet);
+        sheetBehavior = BottomSheetBehavior.from(layoutBottomSheet);
+        testButton = (Button)findViewById(R.id.test_button);
+
+        // set up bottom sheet
+        setBottomSheetCallback();
+        setOnClickForTestButton();
 
     }
 
@@ -28,11 +46,52 @@ public class MainActivity extends AppCompatActivity {
         testFilterButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
                 FilterDialog filter = new FilterDialog();
                 filter.show(getSupportFragmentManager(), "filters");
+            }
+        });
+    }
+
+    private void setOnClickForTestButton() {
+        testButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (sheetBehavior.getState() != BottomSheetBehavior.STATE_EXPANDED) {
+                    sheetBehavior.setState(BottomSheetBehavior.STATE_EXPANDED);
+                } else {
+                    sheetBehavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
+                }
+            }
+        });
+    }
+
+    private void setBottomSheetCallback() {
+        sheetBehavior.setBottomSheetCallback(new BottomSheetBehavior.BottomSheetCallback() {
+            @Override
+            public void onStateChanged(@NonNull View bottomSheet, int newState) {
+                switch (newState) {
+                    case BottomSheetBehavior.STATE_HIDDEN:
+                        break;
+                    case BottomSheetBehavior.STATE_EXPANDED: {
+                        testButton.setText("Close");
+                    }
+                    break;
+                    case BottomSheetBehavior.STATE_COLLAPSED: {
+                        testButton.setText("Expand");
+                    }
+                    break;
+                    case BottomSheetBehavior.STATE_DRAGGING:
+                        break;
+                    case BottomSheetBehavior.STATE_SETTLING:
+                        break;
+                }
+            }
+
+            @Override
+            public void onSlide(@NonNull View bottomSheet, float slideOffset) {
 
             }
         });
     }
+
 }
