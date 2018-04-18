42b94ba817f0830b4cca3fa9ae3fc5987a58ba38
DM <djmedora@gmail.com>
2018-04-18 15:28:40 -0700

    started componentifying things, updated gitignore

--- app/src/main/java/cs160/dinestination/MainActivity.java
+++ app/src/main/java/cs160/dinestination/MainActivity.java
@@ -2,6 +2,7 @@ package cs160.dinestination;
 
 import android.app.Dialog;
 import android.app.DialogFragment;
+import android.media.Image;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomSheetBehavior;
 import android.support.design.widget.CoordinatorLayout;
@@ -12,7 +13,9 @@ import android.view.ContextThemeWrapper;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.TimePicker;
 
@@ -20,20 +23,22 @@ public class MainActivity extends AppCompatActivity {
 
     BottomSheetBehavior sheetBehavior;
     LinearLayout layoutBottomSheet;
-    Button testButton;
     TextView testText;
     TimePicker timePicker;
+    ImageView whereToRectangle;
+    RelativeLayout whereToRectangleLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        testButton = (Button)findViewById(R.id.button);
         layoutBottomSheet = findViewById(R.id.bottom_sheet);
         sheetBehavior = BottomSheetBehavior.from(layoutBottomSheet);
-        testText = (TextView)findViewById(R.id.textView);
+//        testText = (TextView)findViewById(R.id.textView);
         timePicker = (TimePicker)findViewById(R.id.time_picker);
+        whereToRectangle = (ImageView)findViewById(R.id.where_to_rectangle);
+        whereToRectangleLayout = (RelativeLayout)findViewById(R.id.where_to_rectangle_layout);
 
         sheetBehavior.setBottomSheetCallback(new BottomSheetBehavior.BottomSheetCallback() {
             @Override
@@ -42,11 +47,11 @@ public class MainActivity extends AppCompatActivity {
                     case BottomSheetBehavior.STATE_HIDDEN:
                         break;
                     case BottomSheetBehavior.STATE_EXPANDED: {
-                        testButton.setText("Close");
+//                        testButton.setText("Close");
                     }
                     break;
                     case BottomSheetBehavior.STATE_COLLAPSED: {
-                        testButton.setText("Expand");
+//                        testButton.setText("Expand");
                     }
                     break;
                     case BottomSheetBehavior.STATE_DRAGGING:
@@ -61,19 +66,24 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        testButton.setOnClickListener(new View.OnClickListener() {
+        whereToRectangle.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 if (sheetBehavior.getState() != BottomSheetBehavior.STATE_EXPANDED) {
                     sheetBehavior.setState(BottomSheetBehavior.STATE_EXPANDED);
+                    whereToRectangleLayout.setVisibility(View.VISIBLE);
                 } else {
                     sheetBehavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
+                    whereToRectangleLayout.setVisibility(View.INVISIBLE);
+
                     int hour = timePicker.getCurrentHour();
                     int min = timePicker.getCurrentMinute();
-                    testText.setText(hour + ":" + min);
+//                    testText.setText(hour + ":" + min);
                 }
             }
         });
+
+
     }
 
 
