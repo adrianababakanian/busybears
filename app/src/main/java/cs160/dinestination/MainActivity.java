c0d32c05e42ccb801d875981f34089b9da118581
DM <djmedora@gmail.com>
2018-04-18 22:08:37 -0700

    adding animations

--- app/src/main/java/cs160/dinestination/MainActivity.java
+++ app/src/main/java/cs160/dinestination/MainActivity.java
@@ -15,6 +15,11 @@ import android.util.Log;
 import android.view.ContextThemeWrapper;
 import android.view.Gravity;
 import android.view.View;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.view.animation.AnimationSet;
+import android.view.animation.AnimationUtils;
+import android.view.animation.DecelerateInterpolator;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -32,6 +37,7 @@ public class MainActivity extends AppCompatActivity {
     ConstraintLayout topInputElement;
     ImageView closeTopInputElement;
     ImageView addTopInputElement;
+    CoordinatorLayout rootLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,8 +51,18 @@ public class MainActivity extends AppCompatActivity {
         topInputElement = (ConstraintLayout)findViewById(R.id.top_input_element);
         closeTopInputElement = (ImageView)findViewById(R.id.close_top_input_elem);
         addTopInputElement = (ImageView)findViewById(R.id.add_top_input_elem);
+        rootLayout = (CoordinatorLayout)findViewById(R.id.mainRootView);
+//        whereToRectangle.setAnimation();
 
-        CoordinatorLayout rootLayout = (CoordinatorLayout)findViewById(R.id.mainRootView);
+//        Animation fadeIn = new AlphaAnimation(0, 1);
+//        fadeIn.setInterpolator(new DecelerateInterpolator());
+//        fadeIn.setDuration(1000);
+//        AnimationSet inputFadeInSet = new AnimationSet(true);
+//        AnimationSet inputFadeOutSet = new AnimationSet(true);
+//        animation.addAnimation(fadeIn);
+        final Animation fadeIn = AnimationUtils.loadAnimation(getApplicationContext(), android.R.anim.fade_in);
+        final Animation fadeOut = AnimationUtils.loadAnimation(getApplicationContext(), android.R.anim.fade_out);
+//        whereToRectangle.setAnimation(animation);
 
 
         sheetBehavior.setBottomSheetCallback(new BottomSheetBehavior.BottomSheetCallback() {
@@ -56,10 +72,17 @@ public class MainActivity extends AppCompatActivity {
                     case BottomSheetBehavior.STATE_HIDDEN:
                         break;
                     case BottomSheetBehavior.STATE_EXPANDED: {
-//                        testButton.setText("Close");
+                        topInputElement.startAnimation(fadeIn);
+                        whereToRectangle.startAnimation(fadeOut);
+
+                        topInputElement.setVisibility(View.VISIBLE);
+                        whereToRectangle.setVisibility(View.INVISIBLE);
                     }
                     break;
                     case BottomSheetBehavior.STATE_COLLAPSED: {
+                        topInputElement.startAnimation(fadeOut);
+                        whereToRectangle.startAnimation(fadeIn);
+
                         topInputElement.setVisibility(View.INVISIBLE);
                         whereToRectangle.setVisibility(View.VISIBLE);
                     }
@@ -83,20 +106,13 @@ public class MainActivity extends AppCompatActivity {
                 if (sheetBehavior.getState() != BottomSheetBehavior.STATE_EXPANDED) {
                     sheetBehavior.setState(BottomSheetBehavior.STATE_EXPANDED);
 
-                    int topInputElemHeight = topInputElement.getHeight();
-                    int rootHeight = findViewById(R.id.mainRootView).getHeight();
-                    // note: on first click, this topInputElem takes the hardcoded value, not what's set here.
-                    layoutBottomSheet.getLayoutParams().height = rootHeight - topInputElemHeight;
-                    layoutBottomSheet.requestLayout();
-//                    android.R.anim.fade_in
-                    topInputElement.setVisibility(View.VISIBLE);
-                    whereToRectangle.setVisibility(View.INVISIBLE);
+//                    int topInputElemHeight = topInputElement.getHeight();
+//                    int rootHeight = findViewById(R.id.mainRootView).getHeight();
+//                    // note: on first click, this topInputElem takes the hardcoded value, not what's set here.
+//                    layoutBottomSheet.getLayoutParams().height = rootHeight - topInputElemHeight;
+//                    layoutBottomSheet.requestLayout();
                 } else {
                     sheetBehavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
-//                    topInputElement.setVisibility(View.INVISIBLE);
-//                    whereToRectangle.setVisibility(View.VISIBLE);
-
-
                     int hour = timePicker.getCurrentHour();
                     int min = timePicker.getCurrentMinute();
 //                    testText.setText(hour + ":" + min);
@@ -107,12 +123,11 @@ public class MainActivity extends AppCompatActivity {
         closeTopInputElement.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                topInputElement.setVisibility(View.INVISIBLE);
                 sheetBehavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
+
                 // force close soft keyboard, else pushes layout up.. hmm.. this works, but poor animation.
                 InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
                 imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
-//                whereToRectangle.requestFocus();
 
             }
         });
