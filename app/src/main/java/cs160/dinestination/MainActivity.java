29ff242cf3fcd2f1eb5c019ddc189e47bfc5ef04
DM <djmedora@gmail.com>
2018-04-18 18:56:19 -0700

    transitions between whereto and input screen

--- app/src/main/java/cs160/dinestination/MainActivity.java
+++ app/src/main/java/cs160/dinestination/MainActivity.java
@@ -4,11 +4,13 @@ import android.app.Dialog;
 import android.app.DialogFragment;
 import android.media.Image;
 import android.support.annotation.NonNull;
+import android.support.constraint.ConstraintLayout;
 import android.support.design.widget.BottomSheetBehavior;
 import android.support.design.widget.CoordinatorLayout;
 import android.support.v4.widget.NestedScrollView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.ContextThemeWrapper;
 import android.view.Gravity;
 import android.view.View;
@@ -23,10 +25,11 @@ public class MainActivity extends AppCompatActivity {
 
     BottomSheetBehavior sheetBehavior;
     LinearLayout layoutBottomSheet;
-    TextView testText;
     TimePicker timePicker;
     ImageView whereToRectangle;
-    RelativeLayout whereToRectangleLayout;
+    ConstraintLayout topInputElement;
+    ImageView closeTopInputElement;
+    ImageView addTopInputElement;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,10 +38,14 @@ public class MainActivity extends AppCompatActivity {
 
         layoutBottomSheet = findViewById(R.id.bottom_sheet);
         sheetBehavior = BottomSheetBehavior.from(layoutBottomSheet);
-//        testText = (TextView)findViewById(R.id.textView);
         timePicker = (TimePicker)findViewById(R.id.time_picker);
         whereToRectangle = (ImageView)findViewById(R.id.where_to_rectangle);
-        whereToRectangleLayout = (RelativeLayout)findViewById(R.id.where_to_rectangle_layout);
+        topInputElement = (ConstraintLayout)findViewById(R.id.top_input_element);
+        closeTopInputElement = (ImageView)findViewById(R.id.close_top_input_elem);
+        addTopInputElement = (ImageView)findViewById(R.id.add_top_input_elem);
+
+        CoordinatorLayout rootLayout = (CoordinatorLayout)findViewById(R.id.mainRootView);
+
 
         sheetBehavior.setBottomSheetCallback(new BottomSheetBehavior.BottomSheetCallback() {
             @Override
@@ -71,10 +78,19 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 if (sheetBehavior.getState() != BottomSheetBehavior.STATE_EXPANDED) {
                     sheetBehavior.setState(BottomSheetBehavior.STATE_EXPANDED);
-                    whereToRectangleLayout.setVisibility(View.VISIBLE);
+
+                    int topInputElemHeight = topInputElement.getHeight();
+                    int rootHeight = findViewById(R.id.mainRootView).getHeight();
+                    // note: on first click, this just takes the hardcoded value.
+                    layoutBottomSheet.getLayoutParams().height = rootHeight - topInputElemHeight;
+                    layoutBottomSheet.requestLayout();
+
+                    topInputElement.setVisibility(View.VISIBLE);
+                    whereToRectangle.setVisibility(View.INVISIBLE);
                 } else {
                     sheetBehavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
-                    whereToRectangleLayout.setVisibility(View.INVISIBLE);
+//                    topInputElement.setVisibility(View.INVISIBLE);
+//                    whereToRectangle.setVisibility(View.VISIBLE);
 
                     int hour = timePicker.getCurrentHour();
                     int min = timePicker.getCurrentMinute();
@@ -82,6 +98,16 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+
+        closeTopInputElement.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                topInputElement.setVisibility(View.INVISIBLE);
+                sheetBehavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
+                whereToRectangle.setVisibility(View.VISIBLE);
+            }
+        });
+
 
 
     }
